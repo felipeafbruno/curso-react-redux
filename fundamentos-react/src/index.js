c26001428c9453a2e449ef260d9345fba58255d3
felipeafbruno <felipedobru@gmail.com>
2021-02-08 12:53:49 -0300

    Primeiro Componente

--- fundamentos-react/src/index.js
+++ fundamentos-react/src/index.js
@@ -1,17 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom'
+import React from 'react'
 
+import './index.css'
+import Primeiro from './components/basicos/Primeiro'
+
+const el = document.getElementById('root')
+const tag = <strong>Olá React!!!</strong>
+
+// ReactDom -> necessário para inserir os components para a apalicação
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    // JSX
+    <div> 
+        <Primeiro></Primeiro> 
+    </div> 
+, el)
