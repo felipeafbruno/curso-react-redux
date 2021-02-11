4d8f99d11f9e9b3c7a7abb1ccdc006279071a553
felipeafbruno <felipedobru@gmail.com>
2021-02-11 17:45:19 -0300

    React Hook - useEffect - Desafio

--- hooks/src/views/examples/UseEffect.jsx
+++ hooks/src/views/examples/UseEffect.jsx
@@ -9,7 +9,13 @@ function calcFactorial(num) {
     return calcFactorial(n-1) * n
 }
 
+function isEvenOdd(num){
+    const number = parseInt(num)
+    return number % 2 === 0 ? 1 : 0 
+}
+
 const UseEffect = (props) => {
+    //#01
     const [number, setNumber] = useState(1)
     const [factorial, setFactorial] = useState(1)
 
@@ -21,6 +27,13 @@ const UseEffect = (props) => {
         setFactorial(calcFactorial(number))
     }, [number]) 
 
+    // #02
+    const [status, setStatus] = useState('Ímpar')
+
+    useEffect(function () {
+        setStatus(isEvenOdd(number))
+    }, [number])
+
     return (
         <div className="UseEffect">
             <PageTitle
@@ -28,13 +41,22 @@ const UseEffect = (props) => {
                 subtitle="Permite executar efeitos colaterais em componentes funcionais!"
             />
 
-            <SectionTitle title="Exercicío #01"/> 
+            <SectionTitle title="Exercício #01"/> 
             <div className="center">
                 <div>
                     <span className="text">Fatorial: </span>
                     <span className="text red">{factorial === -1 ? "Não existe" : factorial}</span>
                 </div>
                 <input type="number" className="input" value={number} onChange={e => setNumber(e.target.value)}/>
+            </div>
+
+            <SectionTitle title="Exercício #02" />
+            <div className="center">
+                <div>
+                    <span className="text">Status: </span>
+                    <span className="text red">{status === 1 ? "Par" : "Ímpar"}</span>
+                </div>
+                <input className="input" type="number" value={number} onChange={e => setNumber(e.target.value)}/>
             </div>
         </div>
     )
