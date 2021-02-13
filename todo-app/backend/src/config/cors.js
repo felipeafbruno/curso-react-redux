82251df5589f48916374d3290d77afaa370be39a
felipeafbruno <felipedobru@gmail.com>
2021-02-13 18:43:41 -0300

    FrontEnd

--- todo-app/backend/src/config/cors.js
+++ todo-app/backend/src/config/cors.js
@@ -1,9 +1,9 @@
-const { render } = require("./server")
+// const { render } = require("./server")
 
-module.exports = function(req, resp, next) {
+module.exports = function(req, res, next) {
     // '*' permitir que qualquer origem acesse a aplicação
     res.header('Access-Control-Allow-Origin', '*') 
     res.header('Access-Control-Allow-Methods', ['GET, POST, OPTIONS, PUT, PATCH, DELETE'])
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requeted-With, Content-Type, Accept')
     next()
 }
