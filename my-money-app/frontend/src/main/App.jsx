e2c2832bfbb90ee7c4f56cb0139efdc4ebb7d877
felipeafbruno <felipedobru@gmail.com>
2021-02-17 16:44:00 -0300

    Visão geral do Dashboard

--- my-money-app/frontend/src/main/App.jsx
+++ my-money-app/frontend/src/main/App.jsx
@@ -4,13 +4,14 @@ import React from 'react'
 import Header from '../common/template/Header'
 import SideBar from '../common/template/Sidebar'
 import Footer from '../common/template/Footer'
+import Routes from './Routes'
 
 export default props => (
     <div className="wrapper">
         <Header />
         <SideBar />
         <div className="content-wrapper">
-            <h1>Conteudo</h1>
+            <Routes />
         </div>
         <Footer />
     </div>
