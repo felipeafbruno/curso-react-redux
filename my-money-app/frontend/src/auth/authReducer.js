249f4e1c1830594d04ddb09703d2f6790827ab4c
felipeafbruno <felipedobru@gmail.com>
2021-02-20 15:50:57 -0300

    Autenticação FrontEnd - authActions para Login e SignUp, Logout e validação de Token

--- my-money-app/frontend/src/auth/authReducer.js
+++ my-money-app/frontend/src/auth/authReducer.js
@@ -6,7 +6,7 @@ const INITIAL_STATE = {
 
 export default (state = INITIAL_STATE, action) => {
     switch(action.type) {
-        case 'TOKEN_VALICATED': 
+        case 'TOKEN_VALIDATED': 
             if(action.payload) {
                 return { ...state, validToken: true }
             } else {
---
+++ my-money-app/frontend/src/consts.js
@@ -0,0 +1,4 @@
+export default  {
+    API_URL: 'http://localhost:3005/api',
+    OAPI_URL: 'http://localhost:3005/oapi'
+}
